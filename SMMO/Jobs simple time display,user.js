d3c3e0eccdd8c3dae4d5a0321e5382458094fa1e
Oligochrome <55333841+oligochrome@users.noreply.github.com>
2023-03-05 23:27:41 +0000

    added catches for when no job is running

--- SMMO/Jobs simple time display,user.js
+++ SMMO/Jobs simple time display,user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        SMMO - Jobs simple time display - simple-mmo.com
 // @namespace   https://github.com/oligochrome
-// @updateURL   https://raw.githubusercontent.com/oligochrome/Userscripts/master/SMMO/Jobs%20simple%20time%20display%2Cuser.js
+// @updateURL   https://github.com/oligochrome/Userscripts/blob/master/SMMO/Jobs%20simple%20time%20display%2Cuser.js
 // @match       https://web.simple-mmo.com/jobs/view/*
 // @match       https://web.simple-mmo.com/jobs/viewall
 // @match       https://web.simple-mmo.com/home
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Ogliochrome
 // @description 3/1/2023, 4:15:49 PM
 // ==/UserScript==
@@ -17,22 +17,42 @@ var n;
 var minsa;
 var minsb;
 
+function displayEndTime() {
+
+
+    mins = parseInt(minsb, 10) * 60000;
+    var d = new Date(Date.now() + mins);
+    var n = d.toLocaleTimeString();
+
+    if (window.location.href.indexOf("https://web.simple-mmo.com/jobs/view/") !== -1) {
+        minsa.innerText = "Finish in " + minsb + " minutes / " + n;
+    } else {
+        minsa.childNodes[8].innerText = "Finish in " + minsb + " minutes / " + n;
+    }
+}
+
 if (window.location.href === "https://web.simple-mmo.com/jobs/viewall") {
     minsa = document.getElementsByClassName('text-center text-xs  sm:text-sm bg-white rounded-lg px-3 py-4 mt-2 ring-1 ring-black ring-opacity-5 shadow-sm dark:border dark:border-gray-800 dark:bg-gray-950')[0];
-    minsb = minsa.childNodes[8].innerText.split('in ')[1].split(' minutes.')[0];
+    if (minsa === undefined) {
+        console.log("Error: No job running")
+    } else {
+        minsb = minsa.childNodes[8].innerText.split('in ')[1].split(' minutes.')[0];
+        displayEndTime();
+    }
 } else if (window.location.href === "https://web.simple-mmo.com/home") {
     minsa = document.getElementsByClassName('text-center text-xs  sm:text-sm bg-white rounded-lg px-3 py-4 mt-2 ring-1 ring-black ring-opacity-5 shadow-sm dark:border dark:border-gray-800 dark:bg-gray-950')[0];
-    minsb = minsa.childNodes[8].innerText.split('in ')[1].split(' minutes.')[0];
+    if (minsa === undefined) {
+        console.log("Error: No job running")
+    } else {
+        minsb = minsa.childNodes[8].innerText.split('in ')[1].split(' minutes.')[0];
+        displayEndTime();
+    }
 } else {
     minsa = document.getElementsByClassName('px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6')[0].childNodes[7].childNodes[7];
-    minsb = minsa.innerText.split('in ')[1].split(' minutes.')[0];
-}
-mins = parseInt(minsb, 10) * 60000;
-var d = new Date(Date.now() + mins);
-var n = d.toLocaleTimeString();
-
-if (window.location.href.indexOf("https://web.simple-mmo.com/jobs/view/") !== -1) {
-    minsa.innerText = "Finish in " + minsb + " minutes / " + n;
-} else {
-    minsa.childNodes[8].innerText = "Finish in " + minsb + " minutes / " + n;
+    if (minsa === undefined) {
+        console.log("Error: No job running")
+    } else {
+        minsb = minsa.innerText.split('in ')[1].split(' minutes.')[0];
+        displayEndTime();
+    }
 }
