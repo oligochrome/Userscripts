484d2392313c7b2ad45a54574f3a97af84cad89f
Oligochrome <55333841+oligochrome@users.noreply.github.com>
2023-11-06 14:17:45 +0000

    Update SMMO - Lottery Autofill and Streamline.user.js

--- SMMO/SMMO - Lottery Autofill and Streamline.user.js
+++ SMMO/SMMO - Lottery Autofill and Streamline.user.js
@@ -5,12 +5,15 @@
 // @grant       none
 // @version     1.0
 // @author      Ogliochrome
+// @updateURL   https://github.com/oligochrome/Userscripts/raw/master/SMMO/SMMO%20-%20Lottery%20Autofill%20and%20Streamline.user.js
 // @description 10/3/2022, 1:03:31 AM
 
 // ==/UserScript==
 
-let getit = document.body.getElementsByClassName('focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300')[0].value = "1";
+// get input box and autofill with 1
+document.body.getElementsByClassName('focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300')[0].value = "1";
 
+// grab all content boxes
 var boxes = document.getElementsByClassName("px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6");
 
 function hideBoxes() {
