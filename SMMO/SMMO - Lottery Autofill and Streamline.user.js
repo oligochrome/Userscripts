53add34285097ad1db438cc6d40ddbdcc4462ebf
Oligochrome <55333841+oligochrome@users.noreply.github.com>
2023-11-06 14:19:50 +0000

    Update SMMO - Lottery Autofill and Streamline.user.js

--- SMMO/SMMO - Lottery Autofill and Streamline.user.js
+++ SMMO/SMMO - Lottery Autofill and Streamline.user.js
@@ -6,6 +6,7 @@
 // @version     1.2
 // @author      Ogliochrome
 // @updateURL   https://github.com/oligochrome/Userscripts/raw/master/SMMO/SMMO%20-%20Lottery%20Autofill%20and%20Streamline.user.js
+// @downloadURL https://github.com/oligochrome/Userscripts/raw/master/SMMO/SMMO%20-%20Lottery%20Autofill%20and%20Streamline.user.js
 // @description Fill in lottery boxes with 1 and hide unneccesary content boxes
 
 // ==/UserScript==
